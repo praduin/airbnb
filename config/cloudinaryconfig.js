fd4f3f0237a6fdc214c5c195fc00f18a087909a9
praduin <118813325+praduin@users.noreply.github.com>
2025-08-25 18:52:59 +0530

    env updated

--- config/cloudinaryconfig.js
+++ config/cloudinaryconfig.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dzknr5bnc",
-  api_key: "773642642243659",
-  api_secret: "YwRACEYQdQl8hTQaizNCrHs5QEk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
