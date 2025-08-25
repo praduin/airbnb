4aab9705ee5591c43dc6533cbdcd0fcc40474507
praduin <118813325+praduin@users.noreply.github.com>
2025-08-25 19:18:56 +0530

    error resolved

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,12 +1,19 @@
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinaryConfig');
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'airbnb_homes',
-    allowed_formats: ['jpg', 'jpeg', 'png'],
+    folder: "airbnb_homes",
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
 
